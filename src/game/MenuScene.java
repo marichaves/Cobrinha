4d4f0e1c5045e9237c323c537f33f11d67403ad1
Mariana Chaves <79344040+marichaves@users.noreply.github.com>
2023-08-17 21:04:06 -0300

    Enhance MenuScene with Mouse Interaction and Scene Transition
    This commit focuses on improving the `MenuScene` class by introducing mouse interaction and enhancing the user experience:
    
    - **ML (MouseListener) Integration:** Added the `ML` (MouseListener) parameter to the `MenuScene` constructor, allowing for better mouse event handling. This integration paves the way for interactive elements in the scene.
    
    - **Mouse Hover Interaction:** Implemented logic to detect mouse hover events over the `playRect` and `exitRect` regions. When the mouse hovers over these areas, the appropriate images (`playPressed`, `exitPressed`) are displayed, providing immediate visual feedback to the user.
    
    - **Mouse Click Transition:** Extended the logic to respond to mouse clicks within the `playRect`. Now, when the user clicks on the `play` area, the game state changes to the game scene (state 1), opening up a seamless transition between scenes.
    
    - **Mouse Interaction Refinement:** The logic has been refined to ensure that the interaction with the `exitRect` region provides visual feedback when the mouse hovers over it.

--- src/game/MenuScene.java
+++ src/game/MenuScene.java
@@ -3,26 +3,74 @@ package game;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
 
 public class MenuScene extends Scene {
+
 	public KL keyListener;
-	public MenuScene(KL keyListener) {
+	public ML mouseListener;
+	public BufferedImage title, play, playPressed, exit, exitPressed;
+	public Rect playRect, exitRect, titleRect; // Renamed ExitRect to exitRect
+	public BufferedImage playCurrentImage, exitCurrentImage;
+
+	// Constructor for the MenuScene class
+	public MenuScene(KL keyListener, ML mouseListener) {
 		this.keyListener = keyListener;
+		this.mouseListener = mouseListener;
+		try {
+			BufferedImage spritesheet = ImageIO.read(new File("assets/menuSprite.png"));
+			title = spritesheet.getSubimage(0, 242, 960, 240);
+			play = spritesheet.getSubimage(0, 121, 261, 121);
+			playPressed = spritesheet.getSubimage(264, 121, 261, 121);
+			exit = spritesheet.getSubimage(0, 0, 233, 93);
+			exitPressed = spritesheet.getSubimage(264, 0, 233, 93);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		playCurrentImage = play;
+		exitCurrentImage = exit;
+
+		titleRect = new Rect(240, 100, 300, 100);
+		playRect = new Rect(310, 280, 150, 70);
+		exitRect = new Rect(318, 355, 130, 55);
 	}
-	
+
 	@Override
 	public void update(double dt) {
-		if (keyListener.isKeyPressed(KeyEvent.VK_UP)) {
-			System.out.println("Seta pra cima está apertada");
+		if (mouseListener.getX() >= playRect.x && mouseListener.getX() <= playRect.x + playRect.width
+				&& mouseListener.getY() >= playRect.y && mouseListener.getY() <= playRect.y + playRect.height) {
+			playCurrentImage = playPressed;
+			if(mouseListener.isPressed()) {
+				Window.changeState(1);
+			}
+		} else {
+			playCurrentImage = play;
 		}
-		
+
+		if (mouseListener.getX() >= exitRect.x && mouseListener.getX() <= exitRect.x + exitRect.width
+			    && mouseListener.getY() >= exitRect.y && mouseListener.getY() <= exitRect.y + exitRect.height) {
+			    exitCurrentImage = exitPressed;
+			    if(mouseListener.isPressed()) {
+			    	
+			    }
+			} else {
+			    exitCurrentImage = exit;
+			}
 	}
 
 	@Override
 	public void draw(Graphics g) {
-		g.setColor(Color.GREEN);
+		g.setColor(Color.PINK);
 		g.fillRect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
-		
-	}
 
+		g.drawImage(title, (int) titleRect.x, (int) titleRect.y, (int) titleRect.width, (int) titleRect.height, null);
+		g.drawImage(playCurrentImage, (int) playRect.x, (int) playRect.y, (int) playRect.width, (int) playRect.height,
+				null);
+		g.drawImage(exitCurrentImage, (int) exitRect.x, (int) exitRect.y, (int) exitRect.width, (int) exitRect.height,
+				null);
+	}
 }
